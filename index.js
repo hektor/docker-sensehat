fffdcb230af465c8657462a8ba83d42f407607c4
Hektor Misplon <hektor.misplon@pm.me>
2023-04-26 18:24:10 +0100

    Set up post request hello world

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 'use strict'
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const sense = require('sense-hat-led')
 
 const PORT = 8080
 const HOST = '0.0.0.0'
 
 const app = express()
+const json = bodyParser.json()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-  sense.showMessage('Hello world')
+app.post('/message',  json, (req, res) => {
+  const { message, speed, fgColor, bgColor } = req.body
+  sense.showMessage(message, speed, fgColor, bgColor, () => console.log(`Shown message: ${message}`))
+  res.json(req.body)
 })
 
 app.listen(PORT, HOST)
